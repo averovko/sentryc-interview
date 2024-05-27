61fde95295505ffb1dac3f5566a4aa4ea674ec58
Oleksandr Verovko <averovko@gmail.com>
2024-05-27 17:12:44 +0200

    Initial commit

---
+++ src/test/java/com/averovko/sentrycinterview/SentrycInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.averovko.sentrycinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentrycInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
